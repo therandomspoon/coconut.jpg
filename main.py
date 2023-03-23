424a16febd3aa14cba00daca65014e117a7c7c43
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-03-23 19:39:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ dir_path = "/"
 file_name = "coconut.jpg"
 filecounter = 0
 found = False
+file_directory = ""
 for root, dirs, files in os.walk(dir_path):
     for file in files:
-        filecounter =filecounter + 1
+        filecounter += 1
         print(file)
         if file == file_name:
             found = True
+            file_directory = root
             break
+    if found:
+        break
 if found:
-    print("The file was found.")
-    print("files counted", filecounter)
+    print(f"The file '{file_name}' was found in directory '{file_directory}'.")
+    print(f"Total files counted: {filecounter}")
     time.sleep(1000)
 else:
     print("The file was not found.")
